432676e1b11c3de606e0811095087479cfa7d1be
gopinath21CS212 <100063293+gopinath21CS212@users.noreply.github.com>
2024-01-09 20:00:53 +0530

    Time: 1 ms (99.06%), Space: 45.8 MB (10.17%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,26 +1,18 @@
 class Solution {
-    public int fun(int s, int e, int[] nums) {
-        int sum = 0;
-        for (int i = s; i < e; i++) {
-            sum += nums[i];
-        }
-        return sum;
-    }
-
     public int pivotIndex(int[] nums) {
-        int s=0;
-        for(int i=1;i<nums.length;i++){
-            s+=nums[i];
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
         }
-        if(s==0) return 0;
-        for (int i = 0; i < nums.length; i++) {
-            int n1 = fun(0, i, nums);
-            int n2 = fun(i + 1, nums.length, nums);
 
-            if (n1 == n2) {
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
                 return i;
             }
+            leftSum += nums[i];
         }
+
         return -1;
     }
 }
